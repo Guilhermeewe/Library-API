715d1689d31519e2c56d61f7da9cea377ac4d9c8
Guilhermeewe <guilhermesarnaglia2@gmail.com>
2026-04-09 17:02:23 -0300

    API Controller

---
+++ src/main/java/io/github/guilhermeewe/libraryapi/controller/AuthorController.java
@@ -0,0 +1,11 @@
+package io.github.guilhermeewe.libraryapi.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/autores")
+//htp://localhost:8080/autores
+public class AuthorController {
+
+}
