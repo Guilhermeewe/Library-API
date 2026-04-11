1ef2742338e38fa18a83ba5dcce458a3e43581b3
Guilhermeewe <guilhermesarnaglia2@gmail.com>
2026-04-11 15:55:43 -0300

    update Author entity and AuthorController for listeners

--- src/main/java/io/github/guilhermeewe/libraryapi/controller/AuthorController.java
+++ src/main/java/io/github/guilhermeewe/libraryapi/controller/AuthorController.java
@@ -1,11 +1,47 @@
 package io.github.guilhermeewe.libraryapi.controller;
 
+import io.github.guilhermeewe.libraryapi.controller.dto.AuthorDTO;
+import io.github.guilhermeewe.libraryapi.model.Author;
+import io.github.guilhermeewe.libraryapi.service.AuthorService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import org.springframework.web.util.UriComponents;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("/autores")
 //htp://localhost:8080/autores
 public class AuthorController {
+
+
+    private final AuthorService authorService;
+
+    public AuthorController(AuthorService authorService){
+        this.authorService = authorService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> salvar(@RequestBody AuthorDTO author) {
+
+        Author authorEntity = author.mapearParaAuthor();
+        authorService.salvarAuthor(authorEntity);
+
+        URI locationURL = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(authorEntity.getId())
+                .toUri();
+
+        return ResponseEntity.created(locationURL).build();
+
+
+
+    }
 
 }
--- src/main/java/io/github/guilhermeewe/libraryapi/model/Author.java
+++ src/main/java/io/github/guilhermeewe/libraryapi/model/Author.java
@@ -6,12 +6,17 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
 @Entity
+@EntityListeners(AuditingEntityListener.class) //Annotations de auditoria
 @Table(name = "tb_author", schema = "public")
 @Getter
 @Setter
@@ -32,7 +37,17 @@ public class Author {
     @Column(name = "nacionalidade", length = 50, nullable = false)
     private String nacionalidade;
 
-
     @OneToMany(mappedBy = "author", fetch = FetchType.LAZY)
     private List<Livro> livrosList;
+
+    @CreatedDate
+    @Column(name = "data_cadastro")
+    private LocalDateTime dataCadastro;
+
+    @LastModifiedDate
+    @Column(name = "data_atualizacao")
+    private LocalDateTime dataAtualizacao;
+
+    @Column(name = "id_usuario")
+    private UUID idUsuario;
 }
